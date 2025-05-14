e134039a787644c289fd68e548e4b9196289602c
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-05-14 22:17:57 +0200

    basic backend init

---
+++ server/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import "dotenv/config";
+import cors from "cors";
+import http from "http";
+import { connectDB } from './lib/db.js';
+
+// Create Express app and http server
+const app = express();
+const server = http.createServer(app)
+
+// Middleware setup
+app.use(express.json({limit: "4mb"}));
+app.use(cors());
+
+app.use("/api/status", (req, res)=> res.send("Server is live"));
+
+
+// Connect to MongoDB
+await connectDB();
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, ()=> console.log("Server is running on PORT: " + PORT));
