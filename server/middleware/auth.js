43284e29bf124acc6400c62e31e9f49ad141425d
Lian Wiebe <72026264+lwiebe@users.noreply.github.com>
2025-05-18 14:00:55 +0200

    finish connecting backend & frontend

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,7 +3,7 @@ import User from "../models/User.js";
 
 
 // Middleware to protect routes
-export const protectRoute = async (req, resizeBy, next)=>{
+export const protectRoute = async (req, res, next)=>{
     try {
         const token = req.headers.token;
 
